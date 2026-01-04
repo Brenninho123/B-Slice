c5a0aa978361cb67559622ffcf5f60be7e947f1a
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-01-04 16:24:17 -0500

    Atualizar o FlashingState.hx

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -1,106 +1,101 @@
 package states;
 
 import flixel.FlxSubState;
-
+import flixel.FlxG;
+import flixel.text.FlxText;
+import flixel.FlxSprite;
 import flixel.effects.FlxFlicker;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 import lime.app.Application;
 import flixel.addons.transition.FlxTransitionableState;
 
 class FlashingState extends MusicBeatState
 {
-	public static var leftState:Bool = false;
+    public static var leftState:Bool = false;
 
-	#if mobile
-	var warnTextMobile:FlxText;
-	#else
-	var warnText:FlxText;
-	#end
-	
-	override function create()
-	{
-		super.create();
+    var warnText:FlxText;
+    var bg:FlxSprite;
 
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		add(bg);
+    override function create()
+    {
+        super.create();
 
-		#if mobile
-		var guhMobile:String = "Hey, watch out!\n
-		This Mod contains some flashing lights!\n
-		Press A to disable them now or go to Options Menu.\n
-		Press B to ignore this message.\n
-		You've been warned!";
-		#else
-		var guh:String = "Hey, watch out!\n
-		This Mod contains some flashing lights!\n
-		Press ENTER to disable them now or go to Options Menu.\n
-		Press ESCAPE to ignore this message.\n
-		You've been warned!";
-		#end
-		
-		controls.isInSubstate = false; // qhar I hate it
+        // Fundo preto com fade-in suave
+        bg = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+        bg.alpha = 0;
+        add(bg);
+        FlxTween.tween(bg, { alpha: 1 }, 0.5, { ease: FlxEase.quadOut });
 
-		#if mobile
-		warnTextMobile = new FlxText(0, 0, FlxG.width, guhMobile, 32);
-		warnTextMobile.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
-		warnTextMobile.screenCenter(Y);
-		add(warnTextMobile);
-		#else
-		warnText = new FlxText(0, 0, FlxG.width, guh, 32);
-		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
-		warnText.screenCenter(Y);
-		add(warnText);
-		#end
+        // Texto de aviso
+        #if mobile
+        var warningStr:String = "Hey, watch out!\nThis Mod contains flashing lights!\nPress A to disable them or go to Options.\nPress B to ignore.\nYou've been warned!";
+        #else
+        var warningStr:String = "Hey, watch out!\nThis Mod contains flashing lights!\nPress ENTER to disable them or go to Options.\nPress ESCAPE to ignore.\nYou've been warned!";
+        #end
 
-		#if mobile
-		addTouchPad("NONE", "A_B");
-		#end
-	}
+        warnText = new FlxText(0, 0, FlxG.width, warningStr, 32);
+        warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+        warnText.alpha = 0; // Começa invisível
+        warnText.screenCenter(Y);
+        add(warnText);
 
-	override function update(elapsed:Float)
-	{
-		if(!leftState) {
-			var back:Bool = controls.BACK;
-			if (controls.ACCEPT || back) {
-				leftState = true;
-				FlxTransitionableState.skipNextTransIn = true;
-				FlxTransitionableState.skipNextTransOut = true;
-				if(!back) {
-					ClientPrefs.data.flashing = false;
-					ClientPrefs.saveSettings();
-					FlxG.sound.play(Paths.sound('confirmMenu'));
-					
-					#if mobile
-					FlxFlicker.flicker(warnTextMobile, 1, 0.1, false, true, function(flk:FlxFlicker) {
-						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
-							MusicBeatState.switchState(new TitleState());
-						});
-					});
-					#else
-					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
-						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
-							MusicBeatState.switchState(new TitleState());
-						});
-					});
-					#end
-				} else {
-					FlxG.sound.play(Paths.sound('cancelMenu'));
-					
-					#if mobile
-					FlxTween.tween(warnTextMobile, {alpha: 0}, 1, {
-						onComplete: function (twn:FlxTween) {
-							MusicBeatState.switchState(new TitleState());
-						}
-					});
-					#else
-					FlxTween.tween(warnText, {alpha: 0}, 1, {
-						onComplete: function (twn:FlxTween) {
-							MusicBeatState.switchState(new TitleState());
-						}
-					});
-					#end
-				}
-			}
-		}
-		super.update(elapsed);
-	}
+        // Fade-in do texto
+        FlxTween.tween(warnText, { alpha: 1 }, 1, { ease: FlxEase.quadOut });
+
+        controls.isInSubstate = false;
+
+        #if mobile
+        addTouchPad("NONE", "A_B");
+        #end
+    }
+
+    override function update(elapsed:Float)
+    {
+        if (!leftState)
+        {
+            var back:Bool = controls.BACK;
+            if (controls.ACCEPT || back)
+            {
+                leftState = true;
+                FlxTransitionableState.skipNextTransIn = true;
+                FlxTransitionableState.skipNextTransOut = true;
+
+                if (!back)
+                {
+                    ClientPrefs.data.flashing = false;
+                    ClientPrefs.saveSettings();
+                    FlxG.sound.play(Paths.sound('confirmMenu'));
+
+                    // Suaviza a saída com um pequeno flicker + fade
+                    FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker)
+                    {
+                        FlxTween.tween(bg, { alpha: 0 }, 0.5);
+                        FlxTween.tween(warnText, { alpha: 0 }, 0.5, {
+                            onComplete: function(twn:FlxTween)
+                            {
+                                MusicBeatState.switchState(new TitleState());
+                            }
+                        });
+                    });
+                }
+                else
+                {
+                    FlxG.sound.play(Paths.sound('cancelMenu'));
+
+                    // Apenas fade-out suave
+                    FlxTween.tween(bg, { alpha: 0 }, 0.5);
+                    FlxTween.tween(warnText, { alpha: 0 }, 0.5, {
+                        onComplete: function(twn:FlxTween)
+                        {
+                            MusicBeatState.switchState(new TitleState());
+                        }
+                    });
+                }
+            }
+        }
+
+        super.update(elapsed);
+    }
 }
